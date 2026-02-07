3eaaa7e79e4d04c10799b62a3d6a669d2f33ce8e
Xhall1 <gridoracones@gmail.com>
2026-02-06 23:01:51 -0500

    feat(utils): add Embed utility class for standardized styling

--- src/main/java/com/xhall/commands/CommandManager.java
+++ src/main/java/com/xhall/commands/CommandManager.java
@@ -8,7 +8,6 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
